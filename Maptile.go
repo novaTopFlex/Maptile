c07b56bb6d2a3fa3c843ef6caad22756d81079a1
Andrew Kay <andrewkaymars@proton.me>
2025-06-23 19:53:58 -0400

    Create Maptile.go

---
+++ Maptile.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/x/fyne/widget"
+	"github.com/fyne-io/terminal"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Terminal")
+	m := widget.NewMap()
+	t := terminal.New()
+  s := terminal.New()
+
+	go func() {
+		_ = t.RunLocalShell()
+		a.Quit()
+	}()
+	w.Resize(fyne.NewSize(800, 600))
+	w.SetContent(container.NewGridWithColumns(
+		3,
+		t,
+		m,
+    s,
+	))
+	w.ShowAndRun()
+}
