9f41adeb402d2ab73267e63c080e1e4ec83b241d
Andrew Kay <andrewkaymars@proton.me>
2025-06-23 20:06:11 -0400

    Update installPrereqs.sh

--- installPrereqs.sh
+++ installPrereqs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-go install fyne.io/fyne/v2@latest
+go get fyne.io/fyne/v2@latest
 go mod tidy
-go install fyne.io/x/fyne
+go get fyne.io/x/fyne
 go mod tidy
 go install github.com/fyne-io/terminal/cmd/fyneterm@latest
 go mod tidy
